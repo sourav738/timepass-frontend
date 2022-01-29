6fcb722efe90cba60ded33c0c1b8164f06e8f71d
Sourav <souravsingh738@gmail.com>
2022-01-29 20:52:31 +0530

    reactjs

---
+++ src/Component/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Home = () =>{
+return(
+    <>
+    <React.Fragment>
+    <p>Home Page</p>
+    </React.Fragment>
+   
+    </>
+)
+}
+export default Home;
---
+++ src/Routes/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from 'react-router-dom'
+import Home from "../Component/Home";
+const index = () => {
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Router>
+        </>
+    )
+}
+export default index;
