f910855ee785e5c993766ca402d6eee37afa5966
Sourav <souravsingh738@gmail.com>
2022-02-01 00:13:42 +0530

    registration

--- src/Component/Home/index.js
+++ src/Component/Home/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-const Home = () =>{
-return(
-    <>
-    <React.Fragment>
-    <p>Home Page</p>
-    </React.Fragment>
-   
-    </>
-)
+import { Link } from "react-router-dom";
+const Home = () => {
+    return (
+        <>
+            <React.Fragment>
+                <p>Home Page</p>
+                <Link to="/user-login">Login</Link>
+                <Link to="/user-register">Register</Link>
+            </React.Fragment>
+        </>
+    )
 }
 export default Home;
--- src/Pages/Registration.js
+++ src/Pages/Registration.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 const Registration = () => {
+    const [getRegistration, setRegistration] = useState({
+        firstName: "",
+        middleName: "",
+        lastName: ""
+    })
+    const [getFirstName, setFirstName] = useState("");
+    const handleSubmit = () => {
+        console.log(getFirstName);
+    }
     return (
         <>
             <p>Registration</p>
+            <input type="text"
+                name=""
+                placeholder="Enter your First Name"
+                value={getRegistration.firstName}
+                onChange={(event) =>
+                    getRegistration.firstName(event.target.value)
+                }
+            />
+
+            <input key="field2" 
+            name="field2" onChange={({target}) => setRegistration(firstName => ({...firstName,firstName:target.value}))} value={setRegistration.firstName}/>
+     
+            <button onClick={handleSubmit}>Save</button>
         </>
     )
 
