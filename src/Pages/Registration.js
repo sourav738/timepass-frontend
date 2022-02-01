d3db6f51756cbac7d0fd467168c133b942ec1215
Sourav <souravsingh738@gmail.com>
2022-02-01 11:55:29 +0530

    react js

--- src/Pages/Registration.js
+++ src/Pages/Registration.js
@@ -3,27 +3,74 @@ const Registration = () => {
     const [getRegistration, setRegistration] = useState({
         firstName: "",
         middleName: "",
-        lastName: ""
+        lastName: "",
+        email: '',
+        phoneNo: '',
+        password: ''
     })
     const [getFirstName, setFirstName] = useState("");
     const handleSubmit = () => {
-        console.log(getFirstName);
+        console.log({ getRegistration });
     }
     return (
         <>
             <p>Registration</p>
-            <input type="text"
-                name=""
-                placeholder="Enter your First Name"
-                value={getRegistration.firstName}
-                onChange={(event) =>
-                    getRegistration.firstName(event.target.value)
-                }
-            />
+            <div>
+                <span><label>First Name</label></span>
+                <input type="text"
+                    name=""
+                    placeholder="Enter your First Name"
+                    value={getRegistration.firstName}
+                    onChange={({ target }) => setRegistration(firstName => ({ ...firstName, firstName: target.value }))}
+                />
+            </div>
+            <div>
+                <span><label>Middle Name</label></span>
+                <input type="text"
+                    name=""
+                    placeholder="Enter Your Middle Name"
+                    value={getRegistration.middleName}
+                    onChange={({ target }) => setRegistration(middleName => ({ ...middleName, middleName: target.value }))}
+                />
+            </div>
+            <div>
+                <span><label>Last Name</label></span>
+                <input type="text"
+                    name=""
+                    placeholder="Enter Your Last Name"
+                    value={getRegistration.lastName}
+                    onChange={({ target }) => setRegistration(lastName => ({ ...lastName, lastName: target.value }))}
+                />
+            </div>
+            <div>
+                <span><label>Email</label></span>
+                <input type="text"
+                    name=""
+                    placeholder="Enter Your Email"
+                    value={getRegistration.email}
+                    onChange={({ target }) => setRegistration(email => ({ ...email, email: target.value }))}
+                />
+            </div>
+            <div>
+                <span><label>Phone No</label></span>
+                <input key="field2"
+                    name="field2"
+                    placeholder="Enter Your Phone No"
+                    onChange={({ target }) => setRegistration(phoneNo => ({ ...phoneNo, phoneNo: target.value }))}
+                    value={setRegistration.phoneNo} />
+            </div>
 
-            <input key="field2" 
-            name="field2" onChange={({target}) => setRegistration(firstName => ({...firstName,firstName:target.value}))} value={setRegistration.firstName}/>
-     
+            <div>
+                <span><label>Password</label></span>
+                <input key="field2"
+                    type="password"
+                    name="field2"
+                    placeholder="Enter Your Password"
+                    onChange={({ target }) => setRegistration(password => ({ ...password, password: target.value }))}
+                    value={setRegistration.password} />
+            </div>
+
+
             <button onClick={handleSubmit}>Save</button>
         </>
     )
