2c08b988a040ecae209b1704d822e7ccd22f4d18
Sourav <souravsingh738@gmail.com>
2022-02-07 17:23:31 +0530

    react

--- src/Component/Home/index.js
+++ src/Component/Home/index.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import ReactCrop from 'react-image-crop';
+import ImageCropper from "../ImageCropper";
 const Home = () => {
     const [getLoggedIn, setLoggedIn] = useState(false);
     const userData = JSON.parse(localStorage.getItem('users'));
+    const [imageToCrop, setImageToCrop] = useState(undefined);
+    const [croppedImage, setCroppedImage] = useState(undefined);
     useEffect(() => {
         if (userData) {
             setLoggedIn(true)
@@ -10,8 +14,22 @@ const Home = () => {
     }, [])
     const onHandleLogout = () => {
         localStorage.removeItem('users')
-        setLoggedIn(false)
+        console.log("logout");
     }
+
+    const onUploadFile = (event) => {
+        if (event.target.files && event.target.files.length > 0) {
+            const reader = new FileReader();
+
+            reader.addEventListener('load', () =>
+                setImageToCrop(reader.result)
+            );
+            console.log(event.target.files[0])
+            reader.readAsDataURL(event.target.files[0]);
+        }
+    };
+
+
     return (
         <>
             <React.Fragment>
@@ -25,6 +43,29 @@ const Home = () => {
                 {!getLoggedIn &&
                     <Link to="/user-register">Register</Link>
                 }
+
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={onUploadFile}
+                />
+                <div>
+                    <ImageCropper
+                        imageToCrop={imageToCrop}
+                        onImageCropped={(croppedImage) => setCroppedImage(croppedImage)}
+                    />
+                </div>
+                {
+                    croppedImage &&
+                    <div>
+                        <h2>Cropped Image</h2>
+                        <img
+                            alt="Cropped Image"
+                            src={croppedImage}
+                        />
+                    </div>
+                }
+
             </React.Fragment>
         </>
     )
