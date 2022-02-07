8ca4cdb06748bc2453fc6527febe9b996d2a3dfe
Sourav <souravsingh738@gmail.com>
2022-02-07 13:14:31 +0530

    react

--- src/Authentication/authentication.js
+++ src/Authentication/authentication.js
@@ -2,7 +2,12 @@ import React from 'react'
 import { Routes, Navigate } from 'react-router-dom'
 const Authentication = (props) => {
   const authData = JSON.parse(localStorage.getItem('users'))
-  const isLoggedIn = Boolean(authData.token)
-  return isLoggedIn ? props.children : <Navigate to='/user-login' />
+  if(authData){
+    const isLoggedIn = Boolean(authData.token)
+    return isLoggedIn ? props.children : <Navigate to='/user-login' />
+  }else{
+    return  <Navigate to='/user-login' />
+  }
+ 
 }
 export default Authentication;
--- src/Component/Home/index.js
+++ src/Component/Home/index.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 const Home = () => {
+    const [getLoggedIn, setLoggedIn] = useState(false);
+    const userData = JSON.parse(localStorage.getItem('users'));
+    useEffect(() => {
+        if (userData) {
+            setLoggedIn(true)
+        }
+    }, [])
+    const onHandleLogout = () => {
+        localStorage.removeItem('users')
+        setLoggedIn(false)
+    }
     return (
         <>
             <React.Fragment>
                 <p>Home Page</p>
-                <Link to="/user-login">Login</Link>
-                <Link to="/user-login">Logout</Link>
-                <Link to="/user-register">Register</Link>
+                {!getLoggedIn &&
+                    <Link to="/user-login">Login</Link>
+                }
+                {getLoggedIn &&
+                    <p onClick={onHandleLogout} >Logout</p>
+                }
+                {!getLoggedIn &&
+                    <Link to="/user-register">Register</Link>
+                }
             </React.Fragment>
         </>
     )
